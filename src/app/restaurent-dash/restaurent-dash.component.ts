87b5f435b15dd2e6175d05209a38fa794e70f554
Dileep2357 <97297304+Dileep2357@users.noreply.github.com>
2022-02-14 16:57:21 +0530

    commit

---
+++ src/app/restaurent-dash/restaurent-dash.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { RestaurentData } from './restaurent.model';
+
+@Component({
+  selector: 'app-restaurent-dash',
+  templateUrl: './restaurent-dash.component.html',
+  styleUrls: ['./restaurent-dash.component.css']
+})
+export class RestaurentDashComponent implements OnInit {
+
+  formValue!: FormGroup
+  restaurentModelObj: RestaurentData = new RestaurentData;
+  allRestaurentData : any;
+  showAdd!:boolean
+  showbtn!:boolean;
+  constructor(private formBuilder: FormBuilder, private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      address: [''],
+      services: ['']
+    })
+    this.getAllData()
+  }
+  clickAddResto(){
+    this.formValue.reset();
+    this.showAdd= true;
+    this.showbtn= false;
+  }
+  // Now Subscribing our data which is maped via Services..0
+  addResto() {
+    this.restaurentModelObj.name = this.formValue.value.name;
+    this.restaurentModelObj.email = this.formValue.value.email;
+    this.restaurentModelObj.mobile = this.formValue.value.mobile;
+    this.restaurentModelObj.address = this.formValue.value.address;
+    this.restaurentModelObj.services = this.formValue.value.services;
+
+    this.api.postRestaurent(this.restaurentModelObj).subscribe(res => {
+      console.log(res);
+      alert("Restaurent Records Added Successfull");
+      //clear fill form data
+      let ref = document.getElementById('clear');
+      ref?.click();
+
+      this.formValue.reset();
+      this.getAllData(); // when you post any data
+    },
+      err => {
+        alert("Something Went Wrong!")
+      }
+    )
+  }
+
+  // Get all Data
+  getAllData() {
+    this.api.getRestaurent().subscribe(res => {
+      this.allRestaurentData = res;
+    })
+  }
+
+  //delete records
+  deleteResto(data:any){
+    this.api.deleteRestaurent(data.id).subscribe(res=>{
+      alert("Restaurent Records Deleted Successfully")
+      this.getAllData(); //Quick Refresh Data
+    })
+  }
+
+  onEditResto(data:any){
+    this.showAdd= false;
+    this.showbtn= true;
+    this.restaurentModelObj.id = data.id;
+    this.formValue.controls['name'].setValue(data.name);
+    this.formValue.controls['email'].setValue(data.email);
+    this.formValue.controls['mobile'].setValue(data.mobile);
+    this.formValue.controls['address'].setValue(data.address);
+    this.formValue.controls['services'].setValue(data.services);
+  }
+  updateResto(){
+
+    this.restaurentModelObj.name = this.formValue.value.name;
+    this.restaurentModelObj.email = this.formValue.value.email;
+    this.restaurentModelObj.mobile = this.formValue.value.mobile;
+    this.restaurentModelObj.address = this.formValue.value.address;
+    this.restaurentModelObj.services = this.formValue.value.services;
+
+    this.api.updateRestaurent(this.restaurentModelObj,this.restaurentModelObj.id).subscribe(res=>{
+      alert("Restaurent updated successfully");
+      let ref = document.getElementById('clear');
+      ref?.click();
+
+      this.formValue.reset();
+      this.getAllData(); // when you post any data
+    })
+  }
+}
